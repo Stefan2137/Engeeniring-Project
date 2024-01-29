9f4b5e114289a4ef211c1210acbd70f8ef2cbbc9
Stefan2137 <kubazab1@interia.pl>
2024-01-29 22:36:13 +0100

    xD

--- fitapp/VViewModels/MainVeiwViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainVeiwViewModel.swift
-//  fitapp
-//
-//  Created by Kuba Stefański on 17/10/2023.
-//
-
-import Foundation
--- fitapp/VViewModels/SettingViewViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SettingViewViewModel.swift
-//  fitapp
-//
-//  Created by Kuba Stefański on 18/10/2023.
-//
-
-import Foundation
