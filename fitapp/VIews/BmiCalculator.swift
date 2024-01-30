ff087f7123a8c610484c49351c2ca205426d7142
Stefan2137 <kubazab1@interia.pl>
2024-01-30 03:49:20 +0100

    xDDDDD

--- fitapp/VIews/BmiCalculator.swift
+++ fitapp/VIews/BmiCalculator.swift
@@ -1,43 +1,44 @@
-//
-//  BmiCalculator.swift
-//  fitapp
-//
-//  Created by Kuba Stefański on 15/10/2023.
-//
-
 import SwiftUI
 
 struct BmiCalculator: View {
-   @State private var weight = 0.0
-   @State private var height = 0
+    @State private var weight = 0.0
+    @State private var height = 0
+
+    
+    private func calculateBMI() -> Double {
+        let heightInMeters = Double(height) / 100 // Convert
+        return weight / (heightInMeters * heightInMeters)
+    }
+
     var body: some View {
         VStack{
             Text("BMI Calculator")
+
             HStack{
-                TextField("", value: $weight ,format: .number)
+                TextField("", value: $weight, format: .number)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 100, height: 100)
                 Text("KG/LBS")
             }
+
             HStack{
-                TextField("", value: $height ,format: .number)
+                TextField("", value: $height, format: .number)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 100, height: 100)
                 Text("Height in CM")
             }
-            .offset(x: 20,y: -50)
+            .offset(x: 20, y: -50)
+
             TLButton(title: "Calculate", background: .blue, titlebackground: .white) {
-                //
+                let bmi = calculateBMI()
             }
             .frame(width: 300, height: 40)
         }
-        
-        
     }
 }
 
-struct BmiCalculatorView_Previews : PreviewProvider {
-    static var previews: some View{
+struct BmiCalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
         BmiCalculator()
     }
 }
