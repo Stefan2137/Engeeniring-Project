089f5fddffd8fee504dfcf37f777e4814c4360cf
Stefan2137 <kubazab1@interia.pl>
2024-02-03 01:30:44 +0100

    Tylko UI

--- fitappUITests/fitappUITests.swift
+++ fitappUITests/fitappUITests.swift
@@ -9,43 +9,34 @@ import XCTest
 
 final class fitappUITests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
     
     func testLoginVIew() throws
     {
         
        let app = XCUIApplication()
        let collectionViewsQuery = app.collectionViews
-       let usernamefield = collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["Email Address"]/*[[".cells.textFields[\"Email Address\"]",".textFields[\"Email Address\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        XCTAssertTrue(usernamefield.exists)
-        usernamefield.tap()
-        usernamefield.typeText("kubazab1@wp.pl")
+       let emailfield = collectionViewsQuery.textFields["Email Address"]
+        XCTAssertTrue(emailfield.exists) //Testing if email field exist
+        emailfield.tap()
+        emailfield.typeText("kubazab1@wp.pl")
         
         let password = collectionViewsQuery/*@START_MENU_TOKEN@*/.secureTextFields["Password"]/*[[".cells.secureTextFields[\"Password\"]",".secureTextFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        XCTAssertTrue(password.exists)
+        XCTAssertTrue(password.exists) //Testing if password field exist
         password.tap()
         password.typeText("password")
         
         app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
     
-        
-        
         let button = app.buttons["Login in"]
-        XCTAssertTrue(button.exists)
+        XCTAssertTrue(button.exists) //Testing if Login in button exist
         button.tap()
         sleep(5)
        let homeButton = XCUIApplication().tabBars["Tab Bar"].buttons["Home"]
-        XCTAssertTrue(homeButton.exists)
+        XCTAssertTrue(homeButton.exists)//After correct work of login in view testing if main view of application is presented.
                                         
     }
-  
         func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
